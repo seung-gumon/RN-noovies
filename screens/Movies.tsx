bc91defebee085704903d36970777dfbb70cea7b
Seungseok <seungseok@argoskyc.com>
2022-05-16 01:19:20 +0900

    type error modifi

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import {Text} from "react-native";
 import styled from "styled-components/native";
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
 
-
-const Btn = styled.View`
+const Btn = styled.View<any>`
   flex: 1;
-  justify-content : center;
+  justify-content: center;
   align-items: center;
   background-color: white;
   background-color: ${props => props.theme.mainBgColor};
 `;
 
 
-const Movies = ({navigation: {navigate}}) => (
-    <Btn
-        onPress={() => navigate("Stack" , {screen : "Three"})}
-        style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({navigation: {navigate}}) => (
+    <Btn onPress={() => navigate("Stack", {screen: "Three"})}
+         style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Movies</Text>
     </Btn>
 )
